90857e548b541bc4d2afc0163339c175fec5399a
Onojeta Brown <onosbrown.saved@gmail.com>
2024-08-05 17:42:05 +0100

    deploy

--- src/config/vendure-config.ts
+++ src/config/vendure-config.ts
@@ -73,15 +73,7 @@ export const config: VendureConfig = {
 
   // When adding or altering custom field definitions, the database will
   // need to be updated. See the "Migrations" section in README.md.
-  customFields: {
-    Customer: [
-      {
-        name: 'avatar',
-        type: 'relation',
-        entity: Asset,
-      },
-    ],
-  },
+  customFields: {},
 
   plugins: [
     AssetServerPlugin.init({
